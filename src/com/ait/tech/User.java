d9a9955f520ac18ba929ebfb4abdf967f1e12dce
KennethRoe1 <kennethroe93@gmail.com>
2018-03-20 21:28:47 +0000

    addition of necessary javascript and css files and addition of user java class

---
+++ src/com/ait/tech/User.java
@@ -0,0 +1,59 @@
+package com.ait.tech;
+
+import java.sql.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+	private int id;
+	private String email;
+	private String name;
+	private String pass;
+	private String address;
+	private Date dob;
+	private String role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
